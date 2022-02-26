0c52df97cc65fe1933ba60f96cfc6aa3794d74ce
Priyanshu Indra <priyanshuindra3@gmail.com>
2022-02-26 22:04:57 +0530

    main page nearly completed

--- Frontend/waste-cycle/src/Components/Header.jsx
+++ Frontend/waste-cycle/src/Components/Header.jsx
@@ -7,14 +7,6 @@ function Header() {
     
     return <div className="header">
         <img className="logo" src={ReactLogo} alt='logo' />
-        <nav className="navigation-bar">
-            <ul>
-                <li className="nav-item"><a href="">About Us</a></li>
-                <li className="nav-item"><a href="">What we Do</a></li>
-                <li className="nav-item"><a href="">Contact Us</a></li>
-                <li className="nav-item"><a href="">Our Team</a></li>
-            </ul>
-        </nav>
     </div>
 }
 
--- Frontend/waste-cycle/src/Components/MainPage.jsx
+++ Frontend/waste-cycle/src/Components/MainPage.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Header from './Header'
 import './MainPage.css'
+import image from '../images/mainPageImage.jpg'
 
 function MainPage() {
 
     return <div>
         <Header />
-        <h1>Best Way to <span className="green">use</span> your Waste</h1>
+        <div className="main-page-content">
+            <div>
+                <h1 className="main-message-heading">Best Way to <span className="green">use</span> your Waste</h1>
+                <button className="search-button">Search for best way to Recycle your waste</button>
+            </div>
+            <img className="main-content-img" src={image} />
+        </div>
+         
     </div>
 }
 
